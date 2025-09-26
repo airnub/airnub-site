40f62be7323e0540d7f597754697b063bcd865d7
Alan Gunning <alangunning@gmail.com>
2025-09-26 23:30:02 +0100

    ci: accept natural language pr titles

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,1 +1,17 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: {
+    name: '@commitlint/parse',
+    parserOpts: {
+      headerPattern: /^(?:(?<type>[\w-]+)(?:\((?<scope>.*)\))?!?:\s)?(?<subject>.+)$/,
+      headerCorrespondence: ['type', 'scope', 'subject'],
+    },
+  },
+  rules: {
+    'type-empty': [0],
+    'type-case': [0],
+    'type-enum': [0],
+    'subject-empty': [2, 'never'],
+    'subject-case': [0],
+  },
+};
