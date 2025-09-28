1c67e490b9481baacd3b91efca584be884780ef7
Alan Gunning <alangunning@gmail.com>
2025-09-28 14:38:07 +0100

    fix: restore tailwind styling

--- apps/airnub/postcss.config.js
+++ apps/airnub/postcss.config.js
@@ -1,15 +1,6 @@
-const tailwindPlugin = (() => {
-  try {
-    require.resolve("@tailwindcss/postcss");
-    return "@tailwindcss/postcss";
-  } catch (error) {
-    return "tailwindcss";
-  }
-})();
-
 module.exports = {
   plugins: {
-    [tailwindPlugin]: {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 };
