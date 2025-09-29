59ed8f5f865e394314062b13129b1faf8eee4f04
Alan Gunning <alangunning@gmail.com>
2025-09-29 13:28:19 +0100

    chore: use tailwind config alias (#68)

--- apps/speckit/tailwind.config.ts
+++ apps/speckit/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import preset from "../../packages/tailwind-config/tailwind.config";
+import preset from "@airnub/tailwind-config/tailwind.config";
 
 export default {
   presets: [preset],
