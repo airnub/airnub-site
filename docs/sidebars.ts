8331875b479d29c0058bd352ec4594cabc0eed0f
Alan Gunning <alangunning@gmail.com>
2025-09-30 18:20:09 +0100

    docs: clarify branding workflow

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -4,6 +4,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   tutorialSidebar: [
+    'branding',
     'architecture',
     'development',
     'supabase',
